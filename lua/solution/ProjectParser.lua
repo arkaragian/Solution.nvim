9ee4a729748d5ec6368b8cb40fe13ec49c735483
Aris Karagiannidis <akar@miltech.gr>
2023-01-25 01:00:19 +0200

    feat: start implementation of a project parser

---
+++ lua/solution/ProjectParser.lua
@@ -0,0 +1,19 @@
+local xml2lua = require("xml2lua")
+local handler = require("xml2lua.xmlhandler.tree")
+
+local ProjectParser = {}
+
+--- Parses an xml project project file
+ProjectParser.ParseProject = function(filename)
+
+    --Uses a handler that converts the XML to a Lua table
+    local xml = xml2lua.loadFile(filename)
+
+    --Instantiates the XML parser
+    local parser = xml2lua.parser(handler)
+    parser:parse(xml)
+
+    print(vim.inspect(handler.root))
+end
+
+return ProjectParser
