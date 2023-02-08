1913a88d90b5914d8e8071414a8fe623c0a0e62d
Aris Karagiannidis <akar@miltech.gr>
2023-02-08 10:37:45 +0200

    feat: implement new utility functions

--- lua/solution/utils.lua
+++ lua/solution/utils.lua
@@ -9,5 +9,19 @@ utils.Length = function(table)
     return count
 end
 
+utils.StringStartsWith = function(theString, thePattern)
+    -- Takes a chunk equal to the length of the pattern starting
+    -- form the start of the string. If the chunk is equal to the
+    -- pattern then we return true
+   return string.sub(theString,1,string.len(thePattern))==thePattern
+end
+
+utils.StringTrimWhiteSpace = function(s)
+   return s:gsub("^%s+", ""):gsub("%s+$", "")
+end
+
+utils.StringIsNullOrWhiteSpace = function(s)
+end
+
 
 return utils
