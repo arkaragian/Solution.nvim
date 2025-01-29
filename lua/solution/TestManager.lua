e844b2ce8ac624594034fbe5be2420584c9e1700
Aris Karagiannidis <akar@miltech.gr>
2025-01-29 22:47:04 +0200

    feat: continue test discovery implementation

--- lua/solution/TestManager.lua
+++ lua/solution/TestManager.lua
@@ -304,12 +304,17 @@ TestManager.DebugSelectedTest = function()
     local command = "dotnet test --filter Name~" .. TestManager.State.SelectedTest;
 
     local function on_event(jobid, data, event)
-        print("A test is getting debugged! With JobID: ".. jobid .. " Data: " .. data .. " Event: " .. event);
+        if data~=nil then
+            print("A test is getting debugged! With JobID: ".. jobid .. " Data: " .. vim.inspect(data) .. " Event: " .. event);
+            return
+        end
+        print("A test is getting debugged! With JobID: ".. jobid  .. " Event: " .. event);
     end
 
     -- https://phelipetls.github.io/posts/async-make-in-nvim-with-lua/
     local id = vim.fn.jobstart(command,{
         env = {
+            -- TODO: This is a different name on linux
             VSTEST_RUNNER_DEBUG = 1
         },
         on_stderr = on_event,
@@ -325,6 +330,48 @@ TestManager.DebugSelectedTest = function()
         vim.notify("Command or Shell is not Executable", vim.log.levels.ERROR, { title = "Solution.nvim Execute Single Test" })
     end
 
+    -- We got a PID. We now need to open dap and use a configuration
+    --
+    -- Maybe use some code from neotest
+    --   local success, job = pcall(nio.fn.jobstart, spec.command, {
+  --  cwd = spec.cwd,
+  --  env = { ["VSTEST_HOST_DEBUG"] = "1" },
+  --  pty = true,
+  --  on_stdout = function(_, data)
+  --    nio.run(function()
+  --      data_accum:push(table.concat(data, "\n"))
+  --    end)
+
+  --    if not debugStarted then
+  --      for _, output in ipairs(data) do
+  --        dotnet_test_pid = dotnet_test_pid or string.match(output, "Process Id%p%s(%d+)")
+
+  --        if
+  --          string.find(output, "Waiting for debugger attach...")
+  --          or string.find(output, "Please attach debugger")
+  --          or string.find(output, "Process Id:")
+  --        then
+  --          waitingForDebugger = true
+  --        end
+  --      end
+  --      if dotnet_test_pid ~= nil and waitingForDebugger then
+  --        logger.debug("neotest-dotnet: Dotnet test process ID: " .. dotnet_test_pid)
+  --        debugStarted = true
+
+  --        dap.run(vim.tbl_extend("keep", {
+  --          type = spec.dap.adapter_name,
+  --          name = "attach - netcoredbg",
+  --          request = "attach",
+  --          processId = dotnet_test_pid,
+  --        }, spec.dap.args or {}))
+  --      end
+  --    end
+  --  end,
+  --  on_exit = function(_, code)
+  --    result_code = code
+  --    finish_future.set()
+  --  end,
+  --})
 
     local dap = require("dap")
     require("dapui").open()
